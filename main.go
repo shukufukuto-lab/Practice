41276dea151344a2ef42ffec636c11106f0d14b6
shukufukuto@mac <shukufukuto@gmail.com>
2025-02-22 16:45:52 +0900

    test2

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+func getList(C *gin.Context){
+		c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
 func main(){
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context){
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/ping", getList)
 	r.Run()
 }
