f5aeece82a4ff8a9191e76cde8b01878d54123fa
shukufukuto@mac <shukufukuto@gmail.com>
2025-02-22 17:02:53 +0900

    test

--- main.go
+++ main.go
@@ -2,14 +2,29 @@ package main
 
 import "github.com/gin-gonic/gin"
 
-func getList(c *gin.Context){
+func getting(c *gin.Context){
 	c.JSON(200, gin.H{
 		"message": "pong",
 	})
 }
 
+func posting(c *gin.Context) {
+    /*
+       DB操作など
+    */
+    if err != nil{
+        c.String(http.StatusInternalServerError, "Server Error")
+        return
+    }
+    c.JSON(http.StatusCreated, gin.H{
+        "status": "ok",
+    })
+}
+
+
 func main(){
 	r := gin.Default()
-	r.GET("/ping", getList)
+	r.GET("/get", getting)
+	r.POST("/post", posting)
 	r.Run()
 }
